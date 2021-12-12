d4a151a6a2c462dbf4463ae886da268882db5dcc
marijacurcic7 <52069615+marijacurcic7@users.noreply.github.com>
2021-12-12 16:11:26 +0100

    prosirivanje ontologije

--- python-rdf/Aiiso.py
+++ python-rdf/Aiiso.py
@@ -5,6 +5,11 @@ from rdflib.term import URIRef
 class Aiiso(DefinedNamespace):
     _fail = True
 
+    Programme: URIRef  # A Programme is a KnowledgeGrouping that represents a cohesive collection of educational material referred to by the owning organization as a programme
+    Module: URIRef  # A Module is a KnowledgeGrouping that represents a cohesive collection of educational material referred to by the owning organization as a module
     Course: URIRef  # A Course is a KnowledgeGrouping that represents a cohesive collection of educational material referred to by the owning organization as a course
+    Teaches: URIRef # An organization may specify the Knowledge Groupings that it teaches using this property
+    Department: URIRef  # A Department is a group of people recognised by an organization as forming a cohesive group referred to by the organization as a department
+
 
     _NS = Namespace("http://purl.org/vocab/aiiso/schema#")
