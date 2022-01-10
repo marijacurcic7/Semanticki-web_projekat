8fd851571a97fed9b398d0ea7e2556abe0b1ede1
alek98 <alek@spectra-alarm.com>
2022-01-10 12:24:02 +0100

    test fixed

--- front/src/app/service/tests.service.ts
+++ front/src/app/service/tests.service.ts
@@ -1,16 +1,34 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { take } from 'rxjs/operators';
 import { Test } from '../model/test.model';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestsService {
+  headers: HttpHeaders
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService,
+  ) { }
 
-  getSortedTestsByDuration(sort: 'minDuration' | 'maxDuration') {
-    const queryParams = { params: new HttpParams().set('sort', sort)}
-    return this.http.get<Test[]>('http://localhost:8090/query_sorted_tests_by_duration', queryParams)
+  async setHeaders() {
+    const user = await this.auth.user$.pipe(take(1)).toPromise()
+    const authToken = await user?.getIdToken()
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `${authToken}`,
+    })
+    this.headers = headers
+  }
+
+  async getSortedTestsByDuration(sort: 'minDuration' | 'maxDuration') {
+    await this.setHeaders()
+    const params = new HttpParams().set('sort', sort)
+    return this.http.get<Test[]>('http://localhost:8090/query_sorted_tests_by_duration',
+      { headers: this.headers, params: params }).toPromise()
   }
 }
